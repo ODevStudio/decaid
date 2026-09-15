f6fed0d1b4fbf8d895259d0421e13fdf55ce5be7
ODevStudio <ODevStudio@proton.me>
2026-09-15 16:57:57 +0200

    style(test): apply Dart formatter output

--- test/ble_diagnostics_handler_test.dart
+++ test/ble_diagnostics_handler_test.dart
@@ -25,7 +25,9 @@ void main() {
           'generation': 4,
           'nativeIsScanning': false,
         },
-        'cache': [{'deviceId': 'scale-1', 'instanceId': 42}],
+        'cache': [
+          {'deviceId': 'scale-1', 'instanceId': 42},
+        ],
       };
     final devices = DeviceController([ble]);
     await devices.initialize();
@@ -68,10 +70,7 @@ void main() {
       body['ble']['services'][0]['details']['scan']['nativeIsScanning'],
       false,
     );
-    expect(
-      body['ble']['services'][0]['details']['cache'][0]['instanceId'],
-      42,
-    );
+    expect(body['ble']['services'][0]['details']['cache'][0]['instanceId'], 42);
     final peer = body['connection']['peers'].single;
     expect(peer['deviceId'], 'scale-1');
     expect(peer['name'], 'Original Decent Scale');
